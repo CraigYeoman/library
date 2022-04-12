1f177b1e2f049ae21b69c24b321543939954e9c8
CraigYeoman <yeomancraig@gmail.com>
2022-04-11 19:53:11 -0500

    Working onmodal

--- books.js
+++ books.js
@@ -1,16 +1,19 @@
 let myLibrary = [];
 
+const theHobbit = new Book("meow", "meow", "meow", "meow");
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function() {
-         return (title + author + pages + read)
+        return (title + author + pages + read)
     }
-    console.log(Book.info())
+    console.log(this.info());
 }
 
 function addBookToLibrary() {
-    
+    myLibrary.push(Book)
 }
+
