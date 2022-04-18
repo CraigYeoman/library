b7d910505231fe59385d42601b3455e0f9445db8
CraigYeoman <yeomancraig@gmail.com>
2022-04-18 07:03:54 -0500

    loop to populate the library

--- books.js
+++ books.js
@@ -3,21 +3,53 @@ const libraryContainer = document.querySelector('[data-library-container]');
 
 let library = [];
 
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+const theHobbit = new Book("meow", "meow", "meow", "meow");
+const theHobbo = new Book("new", "new", "new", "new");
+library.push(theHobbit);
+library.push(theHobbo);
+
 function populateLibrary() {
-    clearElement(libraryContainer);
+    //clearElement(libraryContainer);
     library.forEach(book => {
         const bookElement = document.createElement('div');
         bookElement.classList.add('book');
+        bookElement.appendChild(libraryContainer);
         const bookInfoElement = document.createElement('div');
-        bookInfoElement.classtList.add('book-info');
+        bookInfoElement.classList.add('book-info');
         bookInfoElement.appendChild(bookElement);
-        
-
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete');
+        bookElement.appendChild(deleteButton);
+        let deleteSvg = document.createElement('img');
+        deleteSvg.src = 'svg/delete.svg';
+        deleteButton.appendChild(deleteSvg);
+        let i = 0;
+        console.log(book.title)
+        while (i < library.length) {
+            let object = library[i];
+            for (const prop in object) {
+                const bookDetails = document.createElement('div');
+                bookDetails.classList.add('book-details');
+                bookDetails.textContent = object[prop];
+                bookInfoElement.appendChild(bookDetails);
+                
+            }
+            i++;
+        }
     }) 
 }
 
-function clearElement(element) {
-
-}
+//function clearElement(element) {
+    //while(element.firstChild){
+    //    element.removeChild(element.firstChild);
+   // }
+//}
 
 populateLibrary();
